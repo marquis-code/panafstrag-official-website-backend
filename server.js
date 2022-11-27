64d7c1eb408a9dc57c9d57c5b171a5455d14bbb7
shuttlersMarquis <marquis.abah@shuttlers.ng>
2022-11-27 05:56:03 +0100

    updaed model

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ const connectDB = require('./database/db');
 connectDB();
 
 // middleware
-app.use(cors({credentials : true}));
+const corsOptions ={
+  // origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(helment());
 app.use(cookieParser());
